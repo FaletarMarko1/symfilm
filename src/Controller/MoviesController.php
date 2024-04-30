4c48ca9d704a9c6353918f5de22b4377790dea09
Marko Faletar <faletar.marko@gmail.com>
2024-04-30 22:32:38 +0200

    learned how to get data from database and a few useful functions

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
+use App\Repository\MovieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,13 +12,26 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class MoviesController extends AbstractController
 {
+    private $entityManager;
+    public function __construct(EntityManagerInterface $entityManager){
+        $this->entityManager = $entityManager;
+    }
     #[Route('/movies', name: 'movies')]
     public function index(): Response
     {
-        $movies = ["Avengers Endgame", "Loki", "Transformers", "King kong"];
+        //findAll() - SELECT * FROM movies;
+        //find() - SELECT * FROM movies WHERE id = 5
+        //findBy() - SELECT * FROM movies ORDER BY id DESC
+        //findOneBy() - SELECT * FROM movies WHERE id=6 AND title = 'Oppenheimer' ORDER BY id DESCs
+        //count() - SELECT COUNT() FROM movies WHERE id = 1
+        //
+        $repository = $this->entityManager->getRepository(Movie::class);
 
-        return $this->render('index.html.twig', array(
-            'movies' => $movies
-        ));
+        $movies = $repository->getClassName();
+
+        dd($movies);
+
+        return $this->render('index.html.twig');
     }
+
 }
