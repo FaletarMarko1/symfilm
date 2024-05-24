986b1abde12602eca3a663ee09e501397607acb6
Marko Faletar <faletar.marko@gmail.com>
2024-05-24 22:41:07 +0200

    added ability to add movies

--- src/Form/MovieFormType.php
+++ src/Form/MovieFormType.php
@@ -4,25 +4,57 @@ namespace App\Form;
 
 use App\Entity\Actor;
 use App\Entity\Movie;
+//use Doctrine\DBAL\Types\IntegerType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 
 class MovieFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('releaseYear')
-            ->add('description')
-            ->add('imagePath')
+            ->add('title', TextType::class, [
+                'attr' => array(
+                    'class' => 'bg-transparent block border-b-2 w-full h-20 text-6xl outline-none',
+                    'placeholder' => 'Enter the title',
+                ),
+                'label' => false,
+            ])
+            ->add('releaseYear', IntegerType::class, [
+                'attr' => array(
+                    'class' => 'bg-transparent block mt-10 border-b-2 w-full h-20 text-6xl outline-none',
+                    'placeholder' => 'Enter the release year',
+                ),
+                'label' => false,
+            ])
+            ->add('description',TextareaType::class, [
+                'attr' => array(
+                    'class' => 'bg-transparent block mt-10 border-b-2 w-full h-60 text-6xl outline-none',
+                    'placeholder' => 'Enter the description',
+                ),
+                'label' => false,
+            ])
+            ->add('imagePath',FileType::class, [
+                'attr' => array(
+                    'class' => 'py-10',
+                ),
+                'label' => false,
+            ])
+            /*
             ->add('actors', EntityType::class, [
                 'class' => Actor::class,
                 'choice_label' => 'id',
                 'multiple' => true,
             ])
+            */
         ;
     }
 
